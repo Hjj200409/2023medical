026d9b091df71a3f68d1385f7dc2761e5b0af6c8
GitHub User <user@example.com>
2025-10-20 16:04:37 +0800

    Update to show personal information: name and student ID

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse
 
-# 定义一个简单的视图函数，返回Hello World响应
+# 定义一个简单的视图函数，显示个人信息
 def hello_world(request):
-    return HttpResponse("<h1>Hello World!</h1><p>Welcome to my Django application.</p>")
+    return HttpResponse("<h1>何佳佳</h1><p>学号：20231201032</p><p>Welcome to my Django application.</p>")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
